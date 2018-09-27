d2451a028bc4d76e94838790e638c957cfc6ffc3
maartenvanderhoef <maarten@blinkist.com>
2018-09-27 21:26:12 +0200

    Addition of a NULL Backend to provide a simple way of disabling backend lookups

---
+++ store/nullstore.go
@@ -0,0 +1,37 @@
+package store
+
+import (
+	"errors"
+)
+
+var _ Store = &NullStore{}
+
+type NullStore struct{}
+
+func NewNullStore() *NullStore {
+	return &NullStore{}
+}
+
+func (s *NullStore) Write(id SecretId, value string) error {
+	return errors.New("Write is not implemented for Null Store")
+}
+
+func (s *NullStore) Read(id SecretId, version int) (Secret, error) {
+	return Secret{}, errors.New("Not implemented for Null Store")
+}
+
+func (s *NullStore) List(service string, includeValues bool) ([]Secret, error) {
+	return []Secret{}, nil
+}
+
+func (s *NullStore) ListRaw(service string) ([]RawSecret, error) {
+	return []RawSecret{}, nil
+}
+
+func (s *NullStore) History(id SecretId) ([]ChangeEvent, error) {
+	return []ChangeEvent{}, nil
+}
+
+func (s *NullStore) Delete(id SecretId) error {
+	return errors.New("Not implemented for Null Store")
+}

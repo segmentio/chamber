segmentio/chamber

commit c82e7fc69d30f34077beec08ced54bed42840a6a

added list sorting options (#95)

cmd/list.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 	"text/tabwriter"
 
@@ -21,10 +22,16 @@ var listCmd = &cobra.Command{
 
 var (
 	withValues bool
+	sortByTime bool
+	sortByUser bool
+	sortByVersion bool
 )
 
 func init() {
 	listCmd.Flags().BoolVarP(&withValues, "expand", "e", false, "Expand parameter list with values")
+	listCmd.Flags().BoolVarP(&sortByTime, "time", "t", false, "Sort by modified time")
+	listCmd.Flags().BoolVarP(&sortByUser, "user", "u", false, "Sort by user")
+	listCmd.Flags().BoolVarP(&sortByVersion, "version", "v", false, "Sort by version")
 	RootCmd.AddCommand(listCmd)
 }
 
@@ -63,6 +70,17 @@ func list(cmd *cobra.Command, args []string) error {
 	}
 	fmt.Fprintln(w, "")
 
+	sort.Sort(ByName(secrets))
+	if sortByTime {
+		sort.Sort(ByTime(secrets))
+	}
+	if sortByUser {
+		sort.Sort(ByUser(secrets))
+	}
+	if sortByVersion {
+		sort.Sort(ByVersion(secrets))
+	}
+
 	for _, secret := range secrets {
 		fmt.Fprintf(w, "%s\t%d\t%s\t%s",
 			key(secret.Meta.Key),
@@ -90,3 +108,23 @@ func key(s string) string {
 	secretKey := tokens[len(tokens)-1]
 	return secretKey
 }
+
+type ByName []store.Secret
+func (a ByName) Len() int { return len(a)}
+func (a ByName) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
+func (a ByName) Less(i, j int) bool { return a[i].Meta.Key < a[j].Meta.Key }
+
+type ByTime []store.Secret
+func (a ByTime) Len() int { return len(a)}
+func (a ByTime) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
+func (a ByTime) Less(i, j int) bool { return a[i].Meta.Created.Before(a[j].Meta.Created) }
+
+type ByUser []store.Secret
+func (a ByUser) Len() int { return len(a)}
+func (a ByUser) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
+func (a ByUser) Less(i, j int) bool { return a[i].Meta.CreatedBy < a[j].Meta.CreatedBy }
+
+type ByVersion []store.Secret
+func (a ByVersion) Len() int { return len(a)}
+func (a ByVersion) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
+func (a ByVersion) Less(i, j int) bool { return a[i].Meta.Version < a[j].Meta.Version }

commit 304fdbaf0ed11f09b3bc8ad433fddaed7b3be757

hotfix for broken build introduced by #95 also gofmt

cmd/list.go
@@ -8,6 +8,7 @@ import (
 	"text/tabwriter"
 
 	"github.com/pkg/errors"
+	"github.com/segmentio/chamber/store"
 	"github.com/spf13/cobra"
 	analytics "gopkg.in/segmentio/analytics-go.v3"
 )
@@ -21,9 +22,9 @@ var listCmd = &cobra.Command{
 }
 
 var (
-	withValues bool
-	sortByTime bool
-	sortByUser bool
+	withValues    bool
+	sortByTime    bool
+	sortByUser    bool
 	sortByVersion bool
 )
 
@@ -110,21 +111,25 @@ func key(s string) string {
 }
 
 type ByName []store.Secret
-func (a ByName) Len() int { return len(a)}
-func (a ByName) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
+
+func (a ByName) Len() int           { return len(a) }
+func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByName) Less(i, j int) bool { return a[i].Meta.Key < a[j].Meta.Key }
 
 type ByTime []store.Secret
-func (a ByTime) Len() int { return len(a)}
-func (a ByTime) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
+
+func (a ByTime) Len() int           { return len(a) }
+func (a ByTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByTime) Less(i, j int) bool { return a[i].Meta.Created.Before(a[j].Meta.Created) }
 
 type ByUser []store.Secret
-func (a ByUser) Len() int { return len(a)}
-func (a ByUser) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
+
+func (a ByUser) Len() int           { return len(a) }
+func (a ByUser) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByUser) Less(i, j int) bool { return a[i].Meta.CreatedBy < a[j].Meta.CreatedBy }
 
 type ByVersion []store.Secret
-func (a ByVersion) Len() int { return len(a)}
-func (a ByVersion) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
+
+func (a ByVersion) Len() int           { return len(a) }
+func (a ByVersion) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByVersion) Less(i, j int) bool { return a[i].Meta.Version < a[j].Meta.Version }
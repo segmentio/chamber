ea568e38f1dbf8c2112f6ace9646a2d165fc1d0a
Alec Jacobs <charles.jacobs@segment.com>
2023-02-13 14:10:14 -0800

    fix(import): add --normalize-keys option

--- cmd/import.go
+++ cmd/import.go
@@ -20,9 +20,11 @@ var (
 		Args:  cobra.ExactArgs(2),
 		RunE:  importRun,
 	}
+	normalizeKeys bool
 )
 
 func init() {
+	importCmd.Flags().BoolVar(&normalizeKeys, "normalize-keys", false, "Normalize keys to match how `chamber write` would handle them. If not specified, keys will be written exactly how they are defined in the import source.")
 	RootCmd.AddCommand(importCmd)
 }
 
@@ -70,6 +72,9 @@ func importRun(cmd *cobra.Command, args []string) error {
 	}
 
 	for key, value := range toBeImported {
+		if normalizeKeys {
+			key = utils.NormalizeKey(key)
+		}
 		secretId := store.SecretId{
 			Service: service,
 			Key:     key,

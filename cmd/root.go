96c69e644e817db174f6731c78c4c8d3d3b9276e
Ryan McKern <ryan.mckern@segment.com>
2023-05-04 14:37:17 -0700

    Update analytics-go import path
    analytics-go has changed their
    import path from "gopkg.in/segmentio/analytics-go.v3" to
    "github.com/segmentio/analytics-go/v3", so we must update accordingly.

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/segmentio/chamber/v2/store"
 	"github.com/spf13/cobra"
-	analytics "gopkg.in/segmentio/analytics-go.v3"
+	analytics "github.com/segmentio/analytics-go/v3"
 )
 
 // Regex's used to validate service and key names
